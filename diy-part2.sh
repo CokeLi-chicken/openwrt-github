18a5b7c32ba95ee128850c1a1a5c79f4adcafe9c
KEITH <76611126+CokeLi-chicken@users.noreply.github.com>
2021-12-29 10:14:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/10.10.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.0.2/g' package/base-files/files/bin/config_generate
 
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
